1483b5fec17ed72bfc01d96438f2da18a012d049
lloydnoone <lloydnoone92@gmail.com>
2020-11-17 15:11:52 +0000

    initial commit

---
+++ src/Sorter.ts
@@ -0,0 +1,18 @@
+class Sorter {
+  
+  constructor(public collection: /*todo*/) {}
+
+  sort(): void {
+    const { length } = this.collection
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const left = this.collection[j]
+          this.collection[j] = this.collection[j + 1]
+          this.collection[j + 1] = left
+        }
+      }
+    }
+  }
+}
